525e1cc3764922ad678bd66db8f02f2189e84ba4
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2021-04-08 01:25:48 +0300

    section component added

---
+++ src/Components/Section/Section.js
@@ -0,0 +1,12 @@
+import styles from './Section.module.css';
+
+const Section = ({ title, children }) => {
+    return (
+        <section className={styles.section}>
+            <h2 className={styles.title}>{title}</h2>
+            {children}
+        </section>
+    );
+};
+
+export default Section;
